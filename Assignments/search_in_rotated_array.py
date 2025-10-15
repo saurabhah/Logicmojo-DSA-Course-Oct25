85c669002fda6cff46aa35fbef7bd74f9a353fb6
Saurabh Harankhedkar <sourabh.a.h.30@gmail.com>
2025-10-15 19:48:44 +0530

    edge cases

--- Assignments/search_in_rotated_array.py
+++ Assignments/search_in_rotated_array.py
@@ -8,22 +8,24 @@ nums = [4,5,6,7,0,1,2] #3,
 target = 0
 def solution(nums,target):
     
-    l = 0
-    r = len(nums)-1
-    while l <= r:
-        mid = (l+r)//2
-        print(mid)
+    left = 0
+    right = len(nums)-1
+    while left <= right:
+        mid = (left+right)//2
+
         if nums[mid] == target:
             return mid
         
-        if nums[l] < nums[mid]:#sorted left part
-            if nums[l] <= target < nums[mid]:
-                r = mid - 1
+        if nums[left] < nums[mid]:#sorted left part
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
             else:
-                l = mid
+                left = mid + 1
         else:
-            if nums[mid] >= target <= nums[r]:
-                l = mid + 1
+            if nums[mid] >= target <= nums[right]:#sorted right part
+                left = mid + 1
+            else:
+                right = mid - 1
     
     return -1
 
